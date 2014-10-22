9449673f43868bad0d4b2e9939722ce82dba6ef8
eddyN <eddy.ndizera@student.uclouvain.be>
2014-10-22 12:27:40 +0200

    correction sender

--- sender.c
+++ sender.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-    int sockfd = 0, 
+    int sockfd = 0;
     struct sockaddr_in6 serv_addr; 
 
     if((sockfd = socket(AF_INET6, SOCK_DGRAM, 17)) < 0) //cree la socket
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
     serv_addr.sin6_family = AF_INET6; //type d'adresse : IPv6 
     serv_addr.sin6_port = htons(5000); //transforme le port en equivalent binaire 
 
-    if(inet_pton(AF_INET6, "::1/128", &serv_addr.sin6_addr)<=0) //transforme l'adresse IPv6 en binaire
+    if(inet_pton(AF_INET6, "::1", &serv_addr.sin6_addr)<=0) //transforme l'adresse IPv6 en binaire
     {
         printf("\n inet_pton error occured\n");
         return 1;
@@ -38,7 +38,7 @@ int main(int argc, char *argv[])
     */
     
     char* msg = "hello"; 
-    sendto(sockfd, (void*) &msg, 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr)); //envoie a l'adresse specifiee
+    sendto(sockfd, (void*) &msg, sizeof(msg),0, (struct sockaddr *)&serv_addr, sizeof(serv_addr)); //envoie a l'adresse specifiee
     close(sockfd); //ferme la connexion 
 
     return 0;
