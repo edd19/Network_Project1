82272c30661bc92dc6ea17937b01047498a7cecc
eddyN <eddy.ndizera@student.uclouvain.be>
2014-10-25 14:07:22 +0200

    packet

--- packet.c
+++ packet.c
@@ -1,26 +1,11 @@
 #include "packet.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
 
-typedef struct { 
-
-  uint8_t type : 3;
-  uint8_t window : 5;
-  uint8_t seq_num; 
-  uint16_t length;
-  uint16_t payload[32];
-  uint32_t CRC; 
-} packet _attribute_((packed));
-
-
-uint16_t get_length(packet pack)
+uint16_t get_length(Packet pack)
 {
   return pack.length;
 }
 
-int is_last(packet pack)
+int is_last(Packet pack)
 {
   if(pack.length < 4096)
     {
--- packet.h
+++ packet.h
@@ -1,9 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdint.h>
+
+
+typedef struct __attribute__((packed)) {
+
+  uint8_t type : 3;
+  uint8_t window : 5;
+  uint8_t seq_num;
+  uint16_t length;
+  char payload[512];
+  uint32_t CRC;
+} Packet;
 
 
 
 //retourne la taille du packet
-uint16_t get_length(packet pack);
+uint16_t get_length(Packet pack);
 
 //permet de savoir si le packet est le dernier a etre envoye
-int is_last(packet pack);
+int is_last(Packet pack);
 
