a9882a454f3bf6613cbed3cf6f269cd4f4e62d06
eddyN <eddy.ndizera@student.uclouvain.be>
2014-10-25 13:17:10 +0200

    sender en ipv6

--- sender.c
+++ sender.c
@@ -1,44 +1,37 @@
+
+/* Sample UDP client */
+
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <netinet/in.h>
-#include <netdb.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <arpa/inet.h> 
 
-#define BUFLEN 512
- #define NPACK 10
-#define PORT 9930
+int main(int argc, char**argv)
+{
+   int sockfd,n;
+   struct sockaddr_in servaddr,cliaddr;
+   char sendline[1000];
+   char recvline[1000];
 
- #define SRV_IP "192.168.1.18"
-      /* diep(), #includes and #defines like in the server */
-     
-       int main(void)
-       {
-         struct sockaddr_in si_other;
-         int s, i, slen=sizeof(si_other);
-         char buf[BUFLEN];
-     
-    
-        if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
-    
-        memset((char *) &si_other, 0, sizeof(si_other));
-        si_other.sin_family = AF_INET;
-        si_other.sin_port = htons(PORT);
-        if (inet_aton(SRV_IP, &si_other.sin_addr)==0) {
-          fprintf(stderr, "inet_aton() failed\n");
-          exit(1);
-        }
-    
-        for (i=0; i<NPACK; i++) {
-          printf("Sending packet %d\n", i);
-          sprintf(buf, "This is packet %d\n", i);
-          if (sendto(s, buf, BUFLEN, 0, (const struct sockaddr *) &si_other, slen)==-1){}
-        }
-    
-        close(s);
-        return 0;
-      }
+   if (argc != 2)
+   {
+      printf("usage:  udpcli <IP address>\n");
+      exit(1);
+   }
+
+   sockfd=socket(AF_INET,SOCK_DGRAM,0);
+
+   bzero(&servaddr,sizeof(servaddr));
+   servaddr.sin_family = AF_INET;
+   servaddr.sin_addr.s_addr=inet_addr(argv[1]);
+   servaddr.sin_port=htons(32000);
+
+   while (fgets(sendline, 10000,stdin) != NULL)
+   {
+      sendto(sockfd,sendline,strlen(sendline),0,
+             (struct sockaddr *)&servaddr,sizeof(servaddr));
+      n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
+      recvline[n]=0;
+      fputs(recvline,stdout);
+   }
+}
